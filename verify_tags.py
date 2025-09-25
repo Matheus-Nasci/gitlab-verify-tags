72a659cfa01e8c742e419ffb0786c1c7efbe89b6
Matheus-Nasci <nasci.olv@gmail.com>
2025-09-25 00:48:59 -0300

    change messages errors and remove comment

--- verify_tags.py
+++ verify_tags.py
@@ -10,15 +10,14 @@ load_dotenv()
 
 def handle_gitlab_error(context: str, error: Exception, exit_on_error: bool = True):
     if isinstance(error, GitlabError):
-        print(f"Erro no {context}: {error.error_message if hasattr(error, 'error_message') else str(error)}")
+        print(f"Erro on {context}: {error.error_message if hasattr(error, 'error_message') else str(error)}")
     else:
-        print(f"Erro inesperado no {context}: {str(error)}")
+        print(f"Unexpected erro on {context}: {str(error)}")
 
     if exit_on_error:
         sys.exit(1)
 
 def parse_args() -> argparse.Namespace:
-    # argumentos para as funções
     parser = argparse.ArgumentParser(description="Verify GitLab tags for production.")
     parser.add_argument(
         "--gitlab-url",
@@ -54,7 +53,6 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 def login_to_gitlab(gitlab_url: str, private_token: str) -> gitlab.Gitlab:
-    # autenticação no GitLab via token
     try:
         gl = gitlab.Gitlab(gitlab_url, private_token=private_token)
         gl.auth()
@@ -71,45 +69,32 @@ def get_commit_from_tag(gl: gitlab.Gitlab, project_id: str, tag: str) -> str:
         handle_gitlab_error("get_commit_from_tag", e)
 
 def check_branch_pipeline(gl: gitlab.Gitlab, project_id: str, commit_sha: str, tag: str, branch: str) -> bool:
-    # pega todas as pipelines com a tag
     try:
         project = gl.projects.get(project_id)
-
-        # pega todos commits da branch
         commits = project.commits.list(ref_name=branch, per_page=100)
-
-        # verifica se o commit da tag existe na branch
         commit_in_branch = any(c.id == commit_sha for c in commits)
 
         if not commit_in_branch:
-            print(f"O commit da tag {tag} não existe na branch {branch}.")
+            print(f"The commit for tag {tag} does not exist on branch {branch}.")
             return False
 
-        # se o commit existe, verifica se alguma pipeline da homologation passou com sucesso
         pipelines = project.pipelines.list(ref="", per_page=100)
         for p in pipelines:
             pipeline = project.pipelines.get(p.id)
             if pipeline.status == "success":
-                print(f"Tag {tag} já passou pela branch {branch} com sucesso.")
+                print(f"Tag {tag} has now successfully passed branch {branch}.")
                 return True
 
-        print(f"Tag {tag} ainda não teve pipeline bem-sucedida em {branch}.")
+        print(f"Tag {tag} has not yet been successfully pipelined into {branch}.")
         return False
 
     except Exception as e:
         handle_gitlab_error("check_homologation_pipeline", e)
 
 def main():
-    # carregando os argumentos do .env ou linha de comando
     args = parse_args()
-
-    # login no GitLab
     gl = login_to_gitlab(args.gitlab_url, args.private_token)
-
-    # pega o commit SHA da tag
     commit_sha = get_commit_from_tag(gl, args.project_id, args.tag_name)
-    
-    # validação da tag 
     check_branch_pipeline(gl, args.project_id, commit_sha, args.tag_name, args.branch_name)
 
 if __name__ == "__main__":
