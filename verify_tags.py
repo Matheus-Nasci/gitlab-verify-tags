14f8c35ca84da24fdd146c5b44fb71f1c680aa69
Matheus-Nasci <nasci.olv@gmail.com>
2025-10-03 10:50:59 -0300

    print text error verify tag deployment

--- verify_tags.py
+++ verify_tags.py
@@ -109,8 +109,8 @@ def main():
     permission_deploy_tag = check_branch_pipeline(gl, args.project_id, commit_sha, args.tag_name, args.branch_name)
 
     if not permission_deploy_tag:
-        print(f"Tag {args.tag_name} is NOT authorized for production deployment.")
-        exit(1)
+        print(f"\033[91mTag {args.tag_name} is NOT authorized for production deployment.\033[0m", file=sys.stderr)
+        raise SystemExit("Pipeline verification failed. Exiting.")
 
 if __name__ == "__main__":
     sys.exit(main())
